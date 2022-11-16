992cce4ac9313e74f2ab3b2e06c8868bdc387ed2
Ushalvn <ushalvnraj1@gmail.com>
2022-11-16 20:57:53 +0800

    Added refresh every 6 seconds

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import pandas as pd
 from main import DataReader
 from data_updater import DataUpdater
 from google.cloud import firestore
+from streamlit_autorefresh import st_autorefresh
 
 
 
@@ -26,26 +27,27 @@ def main():
     col2.metric("Pressure (hPa)", global_vars.PRESSURE_VALS[-1], 0 if len(global_vars.PRESSURE_VALS)<=1 else (global_vars.PRESSURE_VALS[-1]-global_vars.PRESSURE_VALS[-2]))
     col3.metric("Humidity (%)", global_vars.HUMIDITY_VALS[-1], 0 if len(global_vars.HUMIDITY_VALS)<=1 else (global_vars.HUMIDITY_VALS[-1]-global_vars.HUMIDITY_VALS[-2]))
     
-    if st.button('TREND') and (len(global_vars.TEMPERATURE_VALS_PREV)>=25):
-        d1 = {'temp': global_vars.TEMPERATURE_VALS_PREV}
+    
+    if st.button('TREND') and (len(global_vars.TEMPERATURE_VALS)>=20):
+        d1 = {'temp': global_vars.TEMPERATURE_VALS}
         chart_data1 = pd.DataFrame(data=d1)
-        d2 = {'pressure': global_vars.PRESSURE_VALS_PREV}
+        d2 = {'pressure': global_vars.PRESSURE_VALS}
         chart_data2 = pd.DataFrame(data=d2)
-        d3 = {'humidity': global_vars.HUMIDITY_VALS_PREV}
+        d3 = {'humidity': global_vars.HUMIDITY_VALS}
         chart_data3 = pd.DataFrame(data=d3)
-        d4 = {'CO': global_vars.CO_VALS_PREV}
+        d4 = {'CO': global_vars.CO_VALS}
         chart_data4 = pd.DataFrame(data=d4)
-        d5 = {'CO2': global_vars.CO2_VALS_PREV}
+        d5 = {'CO2': global_vars.CO2_VALS}
         chart_data5 = pd.DataFrame(data=d5)
-        d6 = {'NHx': global_vars.NHX_VALS_PREV}
+        d6 = {'NHx': global_vars.NHX_VALS}
         chart_data6= pd.DataFrame(data=d6)
-        d7 = {'PM2.5': global_vars.PM2_point_5_VALS_PREV}
+        d7 = {'PM2.5': global_vars.PM2_point_5_VALS}
         chart_data7 = pd.DataFrame(data=d7)
-        d8 = {'PM10': global_vars.PM10_VALS_PREV}
+        d8 = {'PM10': global_vars.PM10_VALS}
         chart_data8 = pd.DataFrame(data=d8)
-        d9 = {'O3': global_vars.O3_VALS_PREV}
+        d9 = {'O3': global_vars.O3_VALS}
         chart_data9 = pd.DataFrame(data=d9)
-        d10 = {'SO2': global_vars.SO2_VALS_PREV}
+        d10 = {'SO2': global_vars.SO2_VALS}
         chart_data10 = pd.DataFrame(data=d10)
         st.line_chart(chart_data1)
         st.line_chart(chart_data2)
@@ -60,6 +62,7 @@ def main():
     else:
         st.write("Insufficient data to be presented")
 
+    st_autorefresh(interval=6000)
     
 
 
